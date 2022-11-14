e71ec58b173fe5b8e20efa7efffa89ea8fc2416b
Tobias Ullrich <ullrich.tobias@gmx.de>
2022-11-14 23:15:50 +0100

    material-design

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-player',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
+  @Input() name;
   constructor() { }
 
   ngOnInit(): void {
